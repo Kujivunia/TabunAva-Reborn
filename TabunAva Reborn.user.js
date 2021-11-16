d422a91a443718cef2a572c897e0391f35c0eccc
Kujivunia <kujivunia@gmail.com>
2021-11-16 11:55:16 +0600

    Update TabunAva Reborn.user.js

--- TabunAva Reborn.user.js
+++ TabunAva Reborn.user.js
@@ -13,7 +13,7 @@
 //Замена аватарок
 var time = performance.now();
 var time0 = performance.now();
-var Avatar = "https://cdn.everypony.ru/static/local/avatar_" ;//Сокращение ссылок на аватарки
+var Avatar = "https://cdn.everypony.ru/static/local/avatar_"; //Сокращение ссылок на аватарки
 var identicon = "https://avatars.dicebear.com/api/identicon/:";
 var identiconParams = ".svg?scale=100&size=24";
 var avaPost = "https://tabun.everypony.ru/blog/uniblog/203681.html";
@@ -26,63 +26,60 @@ var avatarFemale = Avatar + Avatar + 'female_24x24.png';
 
 fetch(avaPost)
     .then((response) => {
-    return response.text();
-})
+        return response.text();
+    })
     .then((data) => {
-    var parser = new DOMParser();
-    var htmlDocument = parser.parseFromString(data, "text/html");
-    comments = htmlDocument.getElementsByClassName('comment');
-    console.log('Время выполнения загрузки поста для сбора аватарок, мс.: ', performance.now() - time);
-    time = performance.now();
-    //*****************************Выше была какая-то чёрная магия**********************************
-    //Собираю аватарки из ава-поста.
-    [].forEach.call(comments,function(element) {
-        try
-        {
-            if (element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'male_24x24.png') || element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'female_24x24.png')){
-                var temp = element.getElementsByClassName('comment-author');
-                var name = temp[0].children[1].innerText;
-                var tempComment = element.getElementsByClassName('comment-content')[0];
-                if (tempComment.getElementsByClassName('text')[0].getElementsByTagName('img')[0] != null){
-                    if (tempComment.getElementsByClassName('text')[0].getElementsByTagName('img')[0].hasAttribute('src')){
-                        var tempAvaLink = tempComment.getElementsByClassName('text')[0].getElementsByTagName('img')[0].getAttribute('src');
-                        dictUserAva[name] = tempAvaLink;
+        var parser = new DOMParser();
+        var htmlDocument = parser.parseFromString(data, "text/html");
+        comments = htmlDocument.getElementsByClassName('comment');
+        console.log('Время выполнения загрузки поста для сбора аватарок, мс.: ', performance.now() - time);
+        time = performance.now();
+        //*****************************Выше была какая-то чёрная магия**********************************
+        //Собираю аватарки из ава-поста.
+        [].forEach.call(comments, function(element) {
+            try {
+                if (element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'male_24x24.png') || element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'female_24x24.png')) {
+                    var temp = element.getElementsByClassName('comment-author');
+                    var name = temp[0].children[1].innerText;
+                    var tempComment = element.getElementsByClassName('comment-content')[0];
+                    if (tempComment.getElementsByClassName('text')[0].getElementsByTagName('img')[0] != null) {
+                        if (tempComment.getElementsByClassName('text')[0].getElementsByTagName('img')[0].hasAttribute('src')) {
+                            var tempAvaLink = tempComment.getElementsByClassName('text')[0].getElementsByTagName('img')[0].getAttribute('src');
+                            dictUserAva[name] = tempAvaLink;
+                        }
                     }
                 }
-            }
-        }
-        catch(e) {}
+            } catch (e) {}
+        });
+
+        console.log('Время выполнения сбора аватарок, мс.: ', performance.now() - time);
+
+        time = performance.now();
+
+    }).then(() => {
+
+        //Функция, якая выставляет аватарки
+        change_avatar();
+
+        document.getElementById('count-comments').addEventListener('DOMSubtreeModified', function() { //Смотрит число комментариев и при изменении запускает смену аватарок. Без этого новые комментарии будут с обычными аватарками.
+            change_avatar()
+        });
+
     });
 
-    console.log('Время выполнения сбора аватарок, мс.: ', performance.now() - time);
-
-    time = performance.now();
-
-}).then(() => {
-
-    //Функция, якая выставляет аватарки
-    change_avatar();
-
-    document.getElementById('count-comments').addEventListener('DOMSubtreeModified', function() { //Смотрит число комментариев и при изменении запускает смену аватарок. Без этого новые комментарии будут с обычными аватарками.
-        change_avatar() });
-
-});
-
 document.getElementById('count-comments').addEventListener('DOMSubtreeModified', function() { //Смотрит число комментариев и при изменении запускает смену аватарок. Без этого новые комментарии будут с обычными аватарками.
-    change_avatar() });
+    change_avatar()
+});
 
 function change_avatar() {
     //Замена своей аватарки на плашке сверху
     var imgSrc = document.querySelector("#dropdown-user > a:nth-child(1) > img").src;
     var name = document.querySelector("#dropdown-user > a.username").outerText;
-    console.log("="+imgSrc+"="+name);
-    if (/(\/local\/avatar_male_)/.test(imgSrc) || /(\/local\/avatar_female)/.test(imgSrc)){
-        if (dictUserAva[name])
-        {
+    console.log("=" + imgSrc + "=" + name);
+    if (/(\/local\/avatar_male_)/.test(imgSrc) || /(\/local\/avatar_female)/.test(imgSrc)) {
+        if (dictUserAva[name]) {
             document.querySelector("#dropdown-user > a:nth-child(1) > img").src = dictUserAva[name];
-        }
-        else
-        {
+        } else {
             document.querySelector("#dropdown-user > a:nth-child(1) > img").src = identicon + name + identiconParams;
         }
     }
@@ -90,16 +87,13 @@ function change_avatar() {
     time = performance.now();
 
     //Замена аватарок у комментариев внутри поста
-    [].forEach.call(commentInfo,function(element) {
-        if (element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'male_24x24.png') || element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'female_24x24.png')){
+    [].forEach.call(commentInfo, function(element) {
+        if (element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'male_24x24.png') || element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'female_24x24.png')) {
             var temp = element.getElementsByClassName('comment-author');
             var name = temp[0].children[1].innerText;
-            if (dictUserAva[name])
-            {
+            if (dictUserAva[name]) {
                 temp[0].children[0].children[0].src = dictUserAva[name];
-            }
-            else
-            {
+            } else {
                 temp[0].children[0].children[0].src = identicon + name + identiconParams;
             }
         }
@@ -108,17 +102,14 @@ function change_avatar() {
     time = performance.now();
 
     //Замена аватарок у поста
-    [].forEach.call(topicHeader,function(element) {
+    [].forEach.call(topicHeader, function(element) {
         var temp = element.getElementsByClassName('avatar')[0];
         var imgSrcRegExp = new RegExp(temp.getAttribute('src'));
-        if (imgSrcRegExp.test(avatarMale) || imgSrcRegExp.test(avatarFemale)){
+        if (imgSrcRegExp.test(avatarMale) || imgSrcRegExp.test(avatarFemale)) {
             var name = element.getElementsByClassName('topic-info')[0].children[2].outerText;
-            if (dictUserAva[name])
-            {
+            if (dictUserAva[name]) {
                 temp.src = dictUserAva[name];
-            }
-            else
-            {
+            } else {
                 temp.src = identicon + name + identiconParams;
             }
         }
@@ -130,13 +121,10 @@ function change_avatar() {
     var profile = document.getElementById('content');
     name = document.querySelector("#content > div.profile > h2").outerText;
     imgSrc = document.querySelector("#content > div.profile-info-about > a.avatar > img").src;
-    if (/(\/local\/avatar_male_)/.test(imgSrc) || /(\/local\/avatar_female)/.test(imgSrc)){
-        if (dictUserAva[name])
-        {
+    if (/(\/local\/avatar_male_)/.test(imgSrc) || /(\/local\/avatar_female)/.test(imgSrc)) {
+        if (dictUserAva[name]) {
             document.querySelector("#content > div.profile-info-about > a.avatar > img").src = dictUserAva[name];
-        }
-        else
-        {
+        } else {
             document.querySelector("#content > div.profile-info-about > a.avatar > img").src = identicon + name + identiconParams;
         }
     }
@@ -144,20 +132,17 @@ function change_avatar() {
     time = performance.now();
 
     //Замена аватарок у друзей в профиле
-    [].forEach.call(document.querySelector("#content > div.wrapper > div.profile-left > ul.user-list-avatar").getElementsByTagName('li'),function(element) {
+    [].forEach.call(document.querySelector("#content > div.wrapper > div.profile-left > ul.user-list-avatar").getElementsByTagName('li'), function(element) {
         var name = element.children[0].outerText;
         var temp = element.children[0].getElementsByTagName('img')[0];
         var imgSrc = temp.getAttribute('src');
         var imgSrcRegExp = new RegExp(imgSrc);
-        if (/(\/local\/avatar_male_)/.test(imgSrc) || /(\/local\/avatar_female)/.test(imgSrc)){
+        if (/(\/local\/avatar_male_)/.test(imgSrc) || /(\/local\/avatar_female)/.test(imgSrc)) {
             temp.height = 48;
             temp.width = 48;
-            if (dictUserAva[name])
-            {
+            if (dictUserAva[name]) {
                 temp.src = dictUserAva[name];
-            }
-            else
-            {
+            } else {
                 temp.src = identicon + name + ".svg?scale=100&size=48";
             }
         }
