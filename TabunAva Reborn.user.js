61558e8fcfcb9a9b5b47c5d967bdb81ba746a38c
Kujivunia <kujivunia@gmail.com>
2021-11-17 11:34:07 +0600

    Update TabunAva Reborn.user.js
    Нашёлся добрый человек, который полностью переписал скрипт

--- TabunAva Reborn.user.js
+++ TabunAva Reborn.user.js
@@ -1,155 +1,254 @@
 // ==UserScript==
 // @name         TabunAva Reborn
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Установка своего аватара на Табуне!
-// @author       Kujivunia или Niko_de_Andjelo
+// @author       ???
 // @match        https://tabun.everypony.ru/*
 // @grant        none
 // @license MIT
 // ==/UserScript==
 // Я не умею писать скрипты, поэтому "TabunAva" являет собой вольную переделку НЕ моего "Tabun Swarm" https://greasyfork.org/ru/scripts/400907-tabun-swarm
 //IDENTICON: https://avatars.dicebear.com/styles/identicon
-//Замена аватарок
-var time = performance.now();
-var time0 = performance.now();
-var Avatar = "https://cdn.everypony.ru/static/local/avatar_"; //Сокращение ссылок на аватарки
-var identicon = "https://avatars.dicebear.com/api/identicon/:";
-var identiconParams = ".svg?scale=100&size=24";
-var avaPost = "https://tabun.everypony.ru/blog/uniblog/203681.html";
-var commentInfo = document.getElementsByClassName('comment-info');
-var comments = document.getElementsByClassName('comment');
-var dictUserAva = new Object();
-var topicHeader = document.getElementsByClassName('topic-header');
-var avatarMale = Avatar + 'male_24x24.png';
-var avatarFemale = Avatar + Avatar + 'female_24x24.png';
+const everyponyCdnStorageRegex = /(https?:)?\/\/cdn\.everypony\.ru\/storage\//;
+const everyponyCdnStorageLink = '//cdn.everypony.ru/storage/';
+let avaDictionary = {};
 
-fetch(avaPost)
-    .then((response) => {
+function getAvatarsDocument() {
+
+    return fetch('https://tabun.everypony.ru/blog/uniblog/203681.html')
+        .then((response) => {
         return response.text();
     })
-    .then((data) => {
-        var parser = new DOMParser();
-        var htmlDocument = parser.parseFromString(data, "text/html");
-        comments = htmlDocument.getElementsByClassName('comment');
-        console.log('Время выполнения загрузки поста для сбора аватарок, мс.: ', performance.now() - time);
-        time = performance.now();
-        //*****************************Выше была какая-то чёрная магия**********************************
-        //Собираю аватарки из ава-поста.
-        [].forEach.call(comments, function(element) {
-            try {
-                if (element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'male_24x24.png') || element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'female_24x24.png')) {
-                    var temp = element.getElementsByClassName('comment-author');
-                    var name = temp[0].children[1].innerText;
-                    var tempComment = element.getElementsByClassName('comment-content')[0];
-                    if (tempComment.getElementsByClassName('text')[0].getElementsByTagName('img')[0] != null) {
-                        if (tempComment.getElementsByClassName('text')[0].getElementsByTagName('img')[0].hasAttribute('src')) {
-                            var tempAvaLink = tempComment.getElementsByClassName('text')[0].getElementsByTagName('img')[0].getAttribute('src');
-                            dictUserAva[name] = tempAvaLink;
-                        }
-                    }
-                }
-            } catch (e) {}
-        });
-
-        console.log('Время выполнения сбора аватарок, мс.: ', performance.now() - time);
-
-        time = performance.now();
-
-    }).then(() => {
-
-        //Функция, якая выставляет аватарки
-        change_avatar();
-
-        document.getElementById('count-comments').addEventListener('DOMSubtreeModified', function() { //Смотрит число комментариев и при изменении запускает смену аватарок. Без этого новые комментарии будут с обычными аватарками.
-            change_avatar()
-        });
-
+        .then((text) => {
+        let domParser = new DOMParser();
+        return domParser.parseFromString(text, "text/html");
     });
-
-document.getElementById('count-comments').addEventListener('DOMSubtreeModified', function() { //Смотрит число комментариев и при изменении запускает смену аватарок. Без этого новые комментарии будут с обычными аватарками.
-    change_avatar()
-});
-
-function change_avatar() {
-    //Замена своей аватарки на плашке сверху
-    var imgSrc = document.querySelector("#dropdown-user > a:nth-child(1) > img").src;
-    var name = document.querySelector("#dropdown-user > a.username").outerText;
-    console.log("=" + imgSrc + "=" + name);
-    if (/(\/local\/avatar_male_)/.test(imgSrc) || /(\/local\/avatar_female)/.test(imgSrc)) {
-        if (dictUserAva[name]) {
-            document.querySelector("#dropdown-user > a:nth-child(1) > img").src = dictUserAva[name];
-        } else {
-            document.querySelector("#dropdown-user > a:nth-child(1) > img").src = identicon + name + identiconParams;
-        }
-    }
-    console.log('Время выполнения замены своей аватарки на плашке сверху, мс.: ', performance.now() - time);
-    time = performance.now();
-
-    //Замена аватарок у комментариев внутри поста
-    [].forEach.call(commentInfo, function(element) {
-        if (element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'male_24x24.png') || element.getElementsByClassName('comment-author')[0].children[0].children[0].src == (Avatar + 'female_24x24.png')) {
-            var temp = element.getElementsByClassName('comment-author');
-            var name = temp[0].children[1].innerText;
-            if (dictUserAva[name]) {
-                temp[0].children[0].children[0].src = dictUserAva[name];
-            } else {
-                temp[0].children[0].children[0].src = identicon + name + identiconParams;
-            }
-        }
-    });
-    console.log('Время замены аватарок комментариев, мс.: ', performance.now() - time);
-    time = performance.now();
-
-    //Замена аватарок у поста
-    [].forEach.call(topicHeader, function(element) {
-        var temp = element.getElementsByClassName('avatar')[0];
-        var imgSrcRegExp = new RegExp(temp.getAttribute('src'));
-        if (imgSrcRegExp.test(avatarMale) || imgSrcRegExp.test(avatarFemale)) {
-            var name = element.getElementsByClassName('topic-info')[0].children[2].outerText;
-            if (dictUserAva[name]) {
-                temp.src = dictUserAva[name];
-            } else {
-                temp.src = identicon + name + identiconParams;
-            }
-        }
-    });
-    console.log('Время замены аватарок шапок постов, мс.: ', performance.now() - time);
-    time = performance.now();
-
-    //Замена аватарок у профиля
-    var profile = document.getElementById('content');
-    name = document.querySelector("#content > div.profile > h2").outerText;
-    imgSrc = document.querySelector("#content > div.profile-info-about > a.avatar > img").src;
-    if (/(\/local\/avatar_male_)/.test(imgSrc) || /(\/local\/avatar_female)/.test(imgSrc)) {
-        if (dictUserAva[name]) {
-            document.querySelector("#content > div.profile-info-about > a.avatar > img").src = dictUserAva[name];
-        } else {
-            document.querySelector("#content > div.profile-info-about > a.avatar > img").src = identicon + name + identiconParams;
-        }
-    }
-    console.log('Время выполнения замены аватарки профиля, мс.: ', performance.now() - time);
-    time = performance.now();
-
-    //Замена аватарок у друзей в профиле
-    [].forEach.call(document.querySelector("#content > div.wrapper > div.profile-left > ul.user-list-avatar").getElementsByTagName('li'), function(element) {
-        var name = element.children[0].outerText;
-        var temp = element.children[0].getElementsByTagName('img')[0];
-        var imgSrc = temp.getAttribute('src');
-        var imgSrcRegExp = new RegExp(imgSrc);
-        if (/(\/local\/avatar_male_)/.test(imgSrc) || /(\/local\/avatar_female)/.test(imgSrc)) {
-            temp.height = 48;
-            temp.width = 48;
-            if (dictUserAva[name]) {
-                temp.src = dictUserAva[name];
-            } else {
-                temp.src = identicon + name + ".svg?scale=100&size=48";
-            }
-        }
-    });
-    console.log('Время выполнения замены аватарок друзей в профиле, мс.: ', performance.now() - time);
-    time = performance.now();
-    //Замена аватарок в ленте активности
-    //TODO
-
 }
+
+function fillAvatarsDictionary(avaDocument) {
+    const commentNodes = avaDocument.querySelectorAll('#comments .comment');
+
+    commentNodes.forEach((commentNode) => {
+        const authorNode = commentNode.querySelector('.comment-author');
+        const username = authorNode && authorNode.textContent.trim();
+        if (!username) return;
+
+        const contentNode = commentNode.querySelector('.comment-content');
+        const imageNode = contentNode && contentNode.querySelector('.text > img');
+
+        if (imageNode && imageNode.hasAttribute('src')) {
+            avaDictionary[username] = imageNode
+                .getAttribute('src')
+                .replace(everyponyCdnStorageRegex, ''); // Сокращаем количество сохраняемых символов
+        }
+    });
+}
+
+// Сохранить список аватаров в локальное хранилище браузера
+function saveAvatarsDictionary() {
+    const jsonString = JSON.stringify(avaDictionary);
+    localStorage.setItem('TabunAvaReborn_Avatars', jsonString);
+    localStorage.setItem('TabunAvaReborn_LastUpdate', Date.now().toString());
+}
+
+function refreshAvatarsStorage() {
+    return getAvatarsDocument()
+        .then((avaDocument) => {
+        fillAvatarsDictionary(avaDocument);
+        saveAvatarsDictionary();
+    });
+}
+
+function shouldUpdateAvatarsStorage() {
+    const lastUpdate = +(localStorage.getItem('TabunAvaReborn_LastUpdate') || '0');
+    return (Date.now() - lastUpdate) > 1000 * 60 * 10; // прошло больше 10 минут с последнего обновления
+}
+
+// Достать список аватаров из локального хранилища браузера или обновить из поста
+function loadAvatarsDictionary() {
+    if (shouldUpdateAvatarsStorage()) {
+        return refreshAvatarsStorage();
+    } else {
+        const jsonString = localStorage.getItem('TabunAvaReborn_Avatars');
+        if (jsonString) {
+            try {
+                avaDictionary = JSON.parse(jsonString);
+                return Promise.resolve();
+            } catch(e) {
+                return refreshAvatarsStorage();
+            }
+        }
+    }
+}
+
+function isDefaultAvatar(link) {
+    return /(\/local\/avatar_male_)/.test(link) || /(\/local\/avatar_female)/.test(link);
+}
+
+function getIdenticonAvatar(username) {
+    return `https://avatars.dicebear.com/api/identicon/${username}.svg?scale=100&size=48`;
+}
+
+function getNewTabunAvatar(username) {
+    if (avaDictionary[username]) {
+        return everyponyCdnStorageLink + avaDictionary[username];
+    }
+    return false;
+}
+
+function replaceAvatarInImageNode(imageNode, username) {
+    if (getNewTabunAvatar(username)) {
+        imageNode.setAttribute('src', getNewTabunAvatar(username));
+    } else if (
+        !imageNode.getAttribute('src')
+        || isDefaultAvatar(imageNode.getAttribute('src'))
+    ) {
+        imageNode.setAttribute('src', getIdenticonAvatar(username));
+    }
+}
+
+// Замена собственного аватара пользователя в шапке страницы
+function replaceHeaderAvatar() {
+    const imageNode = document.querySelector('#dropdown-user img');
+    if (!imageNode) return;
+
+    const usernameNode = document.querySelector("#dropdown-user .username");
+    const username = usernameNode && usernameNode.textContent.trim();
+    if (!username) return;
+
+    replaceAvatarInImageNode(imageNode, username);
+}
+
+// Замена аватара пользователя в профиле
+function replaceProfileAvatar() {
+    const imageNode = document.querySelector('.profile-info-about img');
+    if (!imageNode) return;
+
+    const usernameNode = document.querySelector(".profile [itemprop=nickname]");
+    const username = usernameNode && usernameNode.textContent.trim();
+    if (!username) return;
+
+    replaceAvatarInImageNode(imageNode, username);
+}
+
+// Замена аватара друзей в профиле
+function replaceProfileFriendAvatars() {
+    const userNodes = document.querySelectorAll('.user-list-avatar > *');
+    userNodes.forEach((userNode) => {
+        const imageNode = userNode.querySelector('img');
+        if (!imageNode) return;
+
+        const username = userNode.textContent.trim();
+        if (!username) return;
+
+        imageNode.width = 48;
+        imageNode.height = 48;
+
+        replaceAvatarInImageNode(imageNode, username);
+    });
+}
+
+// Замена аватара автора поста
+function replaceTopicAuthorAvatars() {
+    const topicNodes = document.querySelectorAll('.topic');
+    topicNodes.forEach((topicNode) => {
+        const imageNode = topicNode.querySelector('.avatar');
+        if (!imageNode) return;
+
+        const usernameNode = document.querySelector("[rel=author]");
+        const username = usernameNode && usernameNode.textContent.trim();
+        if (!username) return;
+
+        replaceAvatarInImageNode(imageNode, username);
+    });
+}
+
+// Замена аватаров в комментариях
+function replaceCommentAvatars() {
+    const commentNodes = document.querySelectorAll('#comments .comment');
+    commentNodes.forEach((commentNode) => {
+        const authorNode = commentNode.querySelector('.comment-author');
+        const username = authorNode && authorNode.textContent.trim();
+        if (!username) return;
+
+        const imageNode = authorNode && authorNode.querySelector('img');
+        if (!imageNode) return;
+
+        replaceAvatarInImageNode(imageNode, username);
+    });
+}
+
+// Замена аватаров в ленте активности
+function replaceStreamAvatars() {
+    const streamNodes = document.querySelectorAll('.stream-item');
+    streamNodes.forEach((streamNode) => {
+        const authorNode = streamNode.querySelector('.info a');
+        const username = authorNode && authorNode.textContent.trim();
+        if (!username) return;
+
+        const imageNode = streamNode.querySelector('img');
+        if (!imageNode) return;
+
+        replaceAvatarInImageNode(imageNode, username);
+    });
+}
+
+// Замена аватаров в разделе "Брони"
+function replacePeopleAvatars() {
+    const userNodes = document.querySelectorAll('.table-users .cell-name');
+    userNodes.forEach((userNode) => {
+        const authorNode = userNode.querySelector('.username');
+        const username = authorNode && authorNode.textContent.trim();
+        if (!username) return;
+
+        const imageNode = userNode.querySelector('img');
+        if (!imageNode) return;
+
+        imageNode.width = 48;
+        imageNode.height = 48;
+
+        replaceAvatarInImageNode(imageNode, username);
+    });
+}
+
+// Замена аватаров в блоке "Пожертвования"
+function replaceDonationAvatars() {
+    const userNodes = document.querySelectorAll('.donation-list > *');
+    userNodes.forEach((userNode) => {
+        const username = userNode.textContent.trim();
+        if (!username) return;
+
+        const imageNode = userNode.querySelector('img');
+        if (!imageNode) return;
+
+        imageNode.width = 24;
+        imageNode.height = 24;
+
+        replaceAvatarInImageNode(imageNode, username);
+    });
+}
+
+function replaceAvatarsOnCommentsRefresh() {
+    const countCommentsNode = document.querySelector('#count-comments');
+    if (!countCommentsNode) return;
+
+    countCommentsNode.addEventListener('DOMSubtreeModified', () => {
+        replaceCommentAvatars();
+    });
+}
+
+
+loadAvatarsDictionary()
+    .then(() => {
+    replaceAvatarsOnCommentsRefresh();
+
+    replaceHeaderAvatar();
+    replaceCommentAvatars();
+    replaceTopicAuthorAvatars();
+    replaceProfileAvatar();
+    replaceProfileFriendAvatars();
+    replaceStreamAvatars();
+    replacePeopleAvatars();
+    replaceDonationAvatars();
+});
